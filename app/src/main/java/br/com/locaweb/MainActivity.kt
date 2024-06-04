097525f5d6b6e565e52ba67489e675ea36a88751
lucas <ramosmanhaes@outlook.com>
2024-06-04 13:35:47 -0300

    Criando estrutura do projeto

---
+++ app/src/main/java/br/com/locaweb/MainActivity.kt
@@ -0,0 +1,34 @@
+package br.com.locaweb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.locaweb.ui.theme.LocawebTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LocawebTheme {
+
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LocawebTheme {
+
+    }
+}
